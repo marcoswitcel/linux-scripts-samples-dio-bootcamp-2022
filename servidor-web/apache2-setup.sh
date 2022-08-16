384b760a1f8a6db8cbf7e6c5441a5b27efc5e3ce
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-08-16 20:42:19 -0300

    adicionando script de setup servidor web

---
+++ servidor-web/apache2-setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+apt install apache2 -y
+
+# Para checar o status pode ser usado o comando abaixo
+cat <<EOF
+
+# Para testar o status do servidor apache2
+systemctl status apache2
+
+EOF
+
+echo "Clonando um projeto Web para exemplo e teste de acesso"
+
+cd /var/www/html/
+git clone https://github.com/marcoswitcel/teapot-basics-of-3d-rendering.git
+
+# Descobrindo possível IP
+IP=$(hostname -I | awk '{print $1}')
+
+cat <<EOF
+
+Aplicação acessível através dos seguintes links:
+*  http://localhost/teapot-basics-of-3d-rendering/
+*  http://$IP/teapot-basics-of-3d-rendering/
+
+EOF
+
