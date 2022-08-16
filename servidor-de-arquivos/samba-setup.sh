4c6553231320f80e3318ac1402a19d560a27f8f7
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-08-15 23:26:04 -0300

    adicionado script de setup do samba

---
+++ servidor-de-arquivos/samba-setup.sh
@@ -0,0 +1,26 @@
+
+FOLDER_NAME=/shared-folder
+
+# Instalando o SAMBA
+sudo apt install samba -y
+
+# Criando e setando as permissões da pasta compartilhada
+mkdir $FOLDER_NAME
+chmod 777 $FOLDER_NAME
+
+cat <<EOF >> /etc/samba/smb.conf
+
+[publico]
+
+path = $FOLDER_NAME
+writable = yes
+guest ok = yes
+guest only = yes
+
+EOF
+
+systemctl restart smbd
+systemctl status smbd
+systemctl enable smbd
+
+
